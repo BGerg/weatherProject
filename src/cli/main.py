9b7dec9780ab7b4b612f61d43084835ea6cc5904
Cof <example&example.com>
2020-12-20 21:41:09 +0100

    basic features to get content and write to json add

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,20 +1,31 @@
 import requests
 import json
 
+def read_json_content():
+    with open('weatherdatas.json') as json_file:
+        original_content = json.load(json_file)
+    return original_content
+
+def set_new_json_content():
+    original_content = read_json_content()
+    temp = original_content['wttr_requests']
+    temp.append(get_wttr_page_content())
+    return original_content
 
 def get_wttr_page_content():
     response = requests.get('http://wttr.in/Miskolc', params=(('format', 'j1'),))
-    return response
+    return json.loads(response.text)
 
-def write_content_to_json_file(response):
-    with open('weatherdatas.json', 'a') as json_file:
-        json.dump(response.json(), json_file, indent=6)
-
-
+def write_content_to_json(response):
+    with open('weatherdatas.json', 'w') as json_file:
+        json.dump(response, json_file, indent=4)
 
 def main():
-    weather_Data = get_wttr_page_content()
-    write_content_to_json_file(weather_Data)
+
+    data = set_new_json_content()
+    write_content_to_json(data)
 
 if __name__ == "__main__":
     main()
+
+
