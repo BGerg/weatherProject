a700de03575d8038ec83be96620eeb1b5b2bf8c6
Cof <example&example.com>
2021-01-19 21:38:20 +0100

    main updated

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,11 +1,15 @@
-from cli.cli_parser import parse
-import heandle_weather_data
+from cli_parser import parse
+from src.core.handle_weather_data import set_new_json_content, get_wttr_page_content, read_json_content
+
+
 
 def main(parse):
 
-    check_file_exist()
-    new_file_content = set_new_json_content()
-    write_content_to_json(new_file_content)
+    app_config = AppConfig(args.city_name, args.json_file)
+    if not isfile(app_config.json_file):
+        create_basic_json_file(app_config.json_file)
+    new_file_content = set_new_json_content(app_config.json_file, app_config.city_name)
+    write_content_to_json(app_config.json_file, new_file_content)
 
 if __name__ == "__main__":
     main(parse)
