f56207ca5534cb43645b9b90503cf7b2d5af732a
Cof <example&example.com>
2021-02-03 22:27:17 +0100

    handle_weather_data tests add

--- weather/core/handle_weather_data.py
+++ weather/core/handle_weather_data.py
@@ -1,16 +1,16 @@
 import requests
 import json
+import sys
+
 
 def get_webpage_content_in_json_format(url_address: str) -> dict:
-    response = requests.get(url_address, params=(('format', 'j1'),))
-    return json.loads(response.text)['current_condition'][0]
-
-def create_basic_json_file(file: str):
-    with open(file, 'w') as new_file:
-        pass
+    try:
+        response = requests.get(url_address, params=(('format', 'j1'),))
+        return json.loads(response.text)['current_condition'][0]
+    except requests.exceptions.RequestException:
+        print("URL not available")
+        sys.exit()
 
 def write_content_to_json(file: str, content: str):
     with open(file, 'a') as output_file:
         json.dump(content, output_file, indent=4)
-
-
---
+++ weather/test/handle_weather_data_tests.py
@@ -0,0 +1,20 @@
+from weather.core.handle_weather_data import write_content_to_json, get_webpage_content_in_json_format
+import unittest
+import os
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.mock_dict = {"FeelsLikeC": "5",
+                          "FeelsLikeF": "41",
+                          "cloudcover": "15",
+                          "humidity": "88",
+                          }
+
+    def test_get_webpage_content_in_json_format(self):
+        self.assertTrue(type(get_webpage_content_in_json_format("http://wttr.in/")) is dict)
+
+    def test_write_content_to_json(self):
+        write_content_to_json("testfile.json", self.mock_dict)
+        self.assertTrue(os.path.exists("testfile.json"))
+        os.remove('testfile.json')
+
